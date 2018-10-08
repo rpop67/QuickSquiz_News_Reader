2bd62ce629c30f9b4b55205067a82f04c24933fb
Akanksha Rajwar <rajwar67@gmail.com>
2018-10-08 10:56:08 +0530

    Implemented news filter

--- app/src/main/java/com/example/android/quicksquiz/Settings.java
+++ app/src/main/java/com/example/android/quicksquiz/Settings.java
@@ -1,7 +1,12 @@
 package com.example.android.quicksquiz;
+import android.content.SharedPreferences;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
     public class Settings extends AppCompatActivity{
 
         @Override
@@ -9,15 +14,44 @@ import android.os.Bundle;
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_settings);
         }
-        public static class SettingsFragment extends PreferenceFragment {
+        public static class SettingsFragment extends PreferenceFragment
+                implements Preference.OnPreferenceChangeListener {
 
             @Override
             public void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 addPreferencesFromResource(R.xml.settings_main);
+                Preference postCount = findPreference(getString(R.string.keyPosts));
+                bindPreferenceSummaryToValue(postCount);
+
+                Preference yourInterested = findPreference(getString(R.string.keyTopic));
+                bindPreferenceSummaryToValue(yourInterested);
+            }
+
+
+           @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String stringValue = newValue.toString();
+                if (preference instanceof ListPreference) {
+                    ListPreference listPreference = (ListPreference) preference;
+                    int prefIndex = listPreference.findIndexOfValue(stringValue);
+                    if (prefIndex >= 0) {
+                        CharSequence[] labels = listPreference.getEntries();
+                        preference.setSummary(labels[prefIndex]);
+                    }
+                } else {
+                    preference.setSummary(stringValue);
+                }
+                return true;
+            }
+
+            private void bindPreferenceSummaryToValue(Preference preference) {
+                preference.setOnPreferenceChangeListener(this);
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+                String preferenceString = preferences.getString(preference.getKey(), "");
+                onPreferenceChange(preference, preferenceString);
             }
         }
-
-     }
+    }
 
 
